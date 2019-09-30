bbcb6441f6547fd0e4aaf5161fb17a0793378a88
kiyodori <kiyodori@yahoo.co.jp>
2019-10-01 06:13:25 +0900

    headerの分離

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -1,21 +1,25 @@
 <!DOCTYPE html>
 <html lang='ja'>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
 
-        <meta name="twitter:card" content="summary_large_image">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        
-        <title>
-            @yield('title')
-        </title>
-    </head>
-    <body>
-        <div class="container">
-            @yield('content')
-        </div>
-    </body>
+    <meta name="twitter:card" content="summary_large_image">
+
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <title>
+        @yield('title')
+    </title>
+</head>
+
+<body>
+    @include('layouts.header')
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
+
 </html>
