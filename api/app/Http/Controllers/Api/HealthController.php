1b46d48310f847db8c5ae6f8a323f2d85f2d8ae8
kiyodori <kiyodori@yahoo.co.jp>
2020-01-16 08:55:07 +0900

    一時的に情報出力

--- api/app/Http/Controllers/Api/HealthController.php
+++ api/app/Http/Controllers/Api/HealthController.php
@@ -9,6 +9,18 @@ class HealthController extends Controller
     // TODO: DB疎通させたほうがよい
     public function index()
     {
-        return response()->json(['health' => 'ok!!'], 200, []);
+        return response()->json(
+            [
+                'health' => 'ok!!',
+                'c' => env('DB_CONNECTION'),
+                'r' => env('DB_READ_HOST'),
+                'r' => env('DB_WRITE_HOST'),
+                'p' => env('DB_PORT'),
+                'd' => env('DB_DATABASE'),
+                'u' => env('DB_USERNAME'),
+                'p' => env('DB_PASSWORD'),
+                's' => env('DB_SOCKET'),
+            ],
+            200, []);
     }
 }
