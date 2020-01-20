78e6db0582daa196ff01ea64723fb28fbd95bd48
kiyodori <kiyodori@yahoo.co.jp>
2020-01-20 15:22:15 +0900

    nohup処理を削除

--- api/docker-entrypoint.sh
+++ api/docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 if [ "$SSM_ACTIVATE" = "true" ]; then
   amazon-ssm-agent -register -code "${ACTIVATE_CODE}" -id "${ACTIVATE_ID}" -region "ap-northeast-1" -y
-  nohup amazon-ssm-agent > /dev/null &
+  amazon-ssm-agent &
 fi
 
 exec /usr/sbin/php-fpm -F
