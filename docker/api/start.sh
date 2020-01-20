1c031a0c263bcea26462eb035d87f4c2cf91c2c1
kiyodori <kiyodori@yahoo.co.jp>
2020-01-20 10:37:46 +0900

    環境変数が反映されるようにするために、php-fpmの起動を別ファイルに切り出し、実行タイミングをずらす

---
+++ docker/api/start.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+
+set -e
+
+if [ "$SSM_ACTIVATE" = "true" ]; then
+  amazon-ssm-agent -register -code "${ACTIVATE_CODE}" -id "${ACTIVATE_ID}" -region "ap-northeast-1" -y
+  nohup amazon-ssm-agent > /dev/null &
+fi
+
+/usr/sbin/php-fpm -F
