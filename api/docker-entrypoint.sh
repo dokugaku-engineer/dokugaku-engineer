cf9973ee3e3a9c858f9d2f67ea225357825d8377
kiyodori <kiyodori@yahoo.co.jp>
2020-01-20 13:34:52 +0900

    ssm-agentを有効化

--- api/docker-entrypoint.sh
+++ api/docker-entrypoint.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-# if [ "$SSM_ACTIVATE" = "true" ]; then
-#   amazon-ssm-agent -register -code "${ACTIVATE_CODE}" -id "${ACTIVATE_ID}" -region "ap-northeast-1" -y
-#   nohup amazon-ssm-agent > /dev/null &
-# fi
+if [ "$SSM_ACTIVATE" = "true" ]; then
+  amazon-ssm-agent -register -code "${ACTIVATE_CODE}" -id "${ACTIVATE_ID}" -region "ap-northeast-1" -y
+  nohup amazon-ssm-agent > /dev/null &
+fi
 
 exec /usr/sbin/php-fpm -F
