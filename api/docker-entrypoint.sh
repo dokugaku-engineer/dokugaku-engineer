5d8fcb40f7682b06665ce0c4d3acaa3fc8b2611c
kiyodori <kiyodori@yahoo.co.jp>
2020-01-21 19:35:26 +0900

    sshでapiコンテナに接続できるようにする

--- api/docker-entrypoint.sh
+++ api/docker-entrypoint.sh
@@ -2,9 +2,14 @@
 
 set -e
 
+root_pw=${ROOT_PW:-rooot}
+ssh-keygen -A
+echo "root:$root_pw" | chpasswd
+/usr/sbin/sshd
+
 # if [ "$SSM_ACTIVATE" = "true" ]; then
 #   amazon-ssm-agent -register -code "${ACTIVATE_CODE}" -id "${ACTIVATE_ID}" -region "ap-northeast-1" -y
 #   amazon-ssm-agent &
 # fi
 
-exec /usr/sbin/php-fpm -F
+/usr/sbin/php-fpm -F
