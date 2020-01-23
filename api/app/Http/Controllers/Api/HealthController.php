5092627918c88b2150eb62d1a70f6b03e702407a
kiyodori <kiyodori@yahoo.co.jp>
2020-01-23 15:48:59 +0900

    デバッグ用の設定を元に戻す

--- api/app/Http/Controllers/Api/HealthController.php
+++ api/app/Http/Controllers/Api/HealthController.php
@@ -12,21 +12,6 @@ class HealthController extends Controller
         return response()->json(
             [
                 'health' => 'ok',
-                'name' => env('APP_NAME'),
-                'env' => env('APP_ENV'),
-                'key' => env('APP_KEY'),
-                'debug' => env('APP_DEBUG'),
-                'url' => env('APP_URL'),
-                'scheme' => env('CLIENT_SCHEME'),
-                'clienturl' => env('CLIENT_URL'),
-                'log' => env('LOG_CHANNEL'),
-                'connection' => env('DB_CONNECTION'),
-                'r' => env('DB_READ_HOST'),
-                'w' => env('DB_WRITE_HOST'),
-                'po' => env('DB_PORT'),
-                'd' => env('DB_DATABASE'),
-                'u' => env('DB_USERNAME'),
-                'ps' => env('DB_PASSWORD'),
             ],
             200, []);
     }
--- api/docker-entrypoint.sh
+++ api/docker-entrypoint.sh
@@ -2,24 +2,11 @@
 
 set -e
 
-root_pw=${ROOT_PW:-rooot}
-ssh-keygen -A
-echo "root:$root_pw" | chpasswd
-/usr/sbin/sshd
-
-echo "test"
-echo ${DB_CONNECTION}
-echo ${DB_READ_HOST}
-echo ${DB_WRITE_HOST}
-echo ${DB_DATABASE}
-echo ${DB_USERNAME}
-echo ${DB_PASSWORD}
-echo ${DB_PORT}
-
-# if [ "$SSM_ACTIVATE" = "true" ]; then
-#   amazon-ssm-agent -register -code "${ACTIVATE_CODE}" -id "${ACTIVATE_ID}" -region "ap-northeast-1" -y
-#   amazon-ssm-agent &
-# fi
+# SSHしたいときはコメントアウトする
+# root_pw=${ROOT_PW:-rooot}
+# ssh-keygen -A
+# echo "root:$root_pw" | chpasswd
+# /usr/sbin/sshd
 
 php artisan config:cache
 
