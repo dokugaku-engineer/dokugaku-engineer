cfb08666cacd38c5c5e317ef0096e0d76712b022
kiyodori <kiyodori@yahoo.co.jp>
2020-01-23 09:44:40 +0900

    環境変数をテスト出力

--- api/docker-entrypoint.sh
+++ api/docker-entrypoint.sh
@@ -7,6 +7,15 @@ ssh-keygen -A
 echo "root:$root_pw" | chpasswd
 /usr/sbin/sshd
 
+echo "test"
+echo ${DB_CONNECTION}
+echo ${DB_READ_HOST}
+echo ${DB_WRITE_HOST}
+echo ${DB_DATABASE}
+echo ${DB_USERNAME}
+echo ${DB_PASSWORD}
+echo ${DB_SOCKET}
+
 # if [ "$SSM_ACTIVATE" = "true" ]; then
 #   amazon-ssm-agent -register -code "${ACTIVATE_CODE}" -id "${ACTIVATE_ID}" -region "ap-northeast-1" -y
 #   amazon-ssm-agent &
