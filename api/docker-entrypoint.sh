67e786fee5162f23ec533758bed6d2a8086210c4
kiyodori <kiyodori@yahoo.co.jp>
2020-02-27 17:10:14 +0900

    デバッグ用コードを削除

--- api/docker-entrypoint.sh
+++ api/docker-entrypoint.sh
@@ -8,13 +8,9 @@ set -e
 # echo "root:$root_pw" | chpasswd
 # /usr/sbin/sshd
 
-echo 0
 if [ ${APP_ENV} = "production" ]; then
-  echo 1
   php artisan config:cache
-  echo 2
   php artisan migrate --force
-  echo 3
 fi
 
 /usr/sbin/php-fpm -F
