0a36361af36008f6bffd8e009483d885d6456d9d
kiyodori <kiyodori@yahoo.co.jp>
2020-03-27 08:40:10 +0900

    クライアントサイドの環境変数を設定

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -75,6 +75,16 @@ export default {
     extend() {}
   },
   /*
+   ** Env for client side
+   */
+  env: {
+    'AUTH0_AUDIENCE': process.env.AUTH0_AUDIENCE,
+    'AUTH0_CLIENT_ID': process.env.AUTH0_CLIENT_ID,
+    'AUTH0_DOMAIN': process.env.AUTH0_DOMAIN,
+    'AUTH0_NAMESPACE': process.env.AUTH0_NAMESPACE,
+    'ORIGIN': process.env.ORIGIN,
+  },
+  /*
    ** Generate configuration
    */
   generate: {
