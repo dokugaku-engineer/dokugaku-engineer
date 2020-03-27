e703907dc5958146bd9d518eee48ae7cf76062f7
kiyodori <kiyodori@yahoo.co.jp>
2020-03-28 07:46:59 +0900

    デバッグ用のコードを追加

--- client/middleware/auth0.js
+++ client/middleware/auth0.js
@@ -14,16 +14,20 @@ export default {
       redirect,
       store
     }) => {
+      console.log(30)
+
       const isAuthenticated = store.state.auth0.isAuthenticated
       const user = store.state.auth0.auth0User
 
       // 未ログインの場合
       if (options.loginRequired && !isAuthenticated) {
+        console.log(31)
         return redirect('/')
       }
 
       // 会員情報が未登録の場合
       if (isAuthenticated && !!user && !user[env.AUTH0_NAMESPACE + 'user_id']) {
+        console.log(32)
         return redirect('/registration')
       }
 
@@ -35,14 +39,18 @@ export default {
             store.state.auth0.permissions
           ).length !== options.requiredPermissions.length
         ) {
+          console.log(33)
           return redirect('/')
         }
       }
 
       // ログイン済みの場合
       if (options.authenticatedRedirectUri && isAuthenticated) {
+        console.log(34)
         return redirect(options.authenticatedRedirectUri)
       }
+
+      console.log(35)
     }
   },
   // registrationページを保護する
@@ -52,7 +60,7 @@ export default {
       redirect,
       store
     }) => {
-      console.log(30)
+      console.log(36)
       console.log(store.state.auth0)
       console.log(store.state.auth0.isAuthenticated)
       const isAuthenticated = store.state.auth0.isAuthenticated
@@ -60,13 +68,17 @@ export default {
 
       // 未ログインの場合
       if (!isAuthenticated) {
+        console.log(37)
         return redirect('/')
       }
 
       // 登録済みの場合
       if (!!user && !!user[env.AUTH0_NAMESPACE + 'user_id']) {
+        console.log(38)
         return redirect('/course/serverside')
       }
+
+      console.log(39)
     }
   }
 }
