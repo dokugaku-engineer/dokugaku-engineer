ce31817d08aaf731dd7c32b4bde0a307b0f82953
kiyodori <kiyodori@yahoo.co.jp>
2020-03-31 19:39:04 +0900

    ログインページへリダイレクトするページを作成

---
+++ client/pages/login/index.vue
@@ -0,0 +1,16 @@
+<script>
+import auth0Middleware from '~/middleware/auth0'
+
+export default {
+  layout: 'none',
+  async fetch({ app }) {
+    const options = {
+      redirect_uri: `${process.env.ORIGIN}/course/serverside`,
+      appState: {
+        targetUrl: '/course/serverside'
+      },
+    }
+    await app.$auth0.loginWithRedirect(options)
+  }
+}
+</script>
