2bc68c2fc3e9373445ca7bceb606dde850a42734
kiyodori <kiyodori@yahoo.co.jp>
2020-04-01 14:31:36 +0900

    退会ページへのリンクを追加

--- client/pages/settings/profile/index.vue
+++ client/pages/settings/profile/index.vue
@@ -17,60 +17,63 @@
       </div>
       
       <div v-else>
-        <form v-if="!loadingUser && !error" @submit.prevent="updateUser()" class="profile-form">
-          <ul v-if="Object.keys(errors).length > 0" class="error-box">
-            <li v-for="(value, key) in errors" :key="key" class="error-box-disc">
-              {{ value[0] }}
-            </li>
-          </ul>
-  
-          <div class="form-group">
-            <label for="users[username]">ユーザー名</label>
-            <input
-              v-model.trim="$v.user.username.$model"
-              type="text"
-              name="users[username]"
-              required
-            >
-            <div v-if="submitError && !$v.user.username.required" class="error-text">
-              ユーザー名は必須です。
+        <div v-if="!loadingUser && !error">
+          <form @submit.prevent="updateUser()" class="profile-form">
+            <ul v-if="Object.keys(errors).length > 0" class="error-box">
+              <li v-for="(value, key) in errors" :key="key" class="error-box-disc">
+                {{ value[0] }}
+              </li>
+            </ul>
+    
+            <div class="form-group">
+              <label for="users[username]">ユーザー名</label>
+              <input
+                v-model.trim="$v.user.username.$model"
+                type="text"
+                name="users[username]"
+                required
+              >
+              <div v-if="submitError && !$v.user.username.required" class="error-text">
+                ユーザー名は必須です。
+              </div>
+              <div v-if="submitError && !$v.user.username.minLength" class="error-text">
+                ユーザー名は3文字以上で入力してください。
+              </div>
+              <div v-if="!$v.user.username.maxLength" class="error-text">
+                ユーザー名は50文字以下で入力してください。
+              </div>
+              <div v-if="!$v.user.username.alphaNumName" class="error-text">
+                ユーザー名は半角英数字及び_, -で入力してください。
+              </div>
             </div>
-            <div v-if="submitError && !$v.user.username.minLength" class="error-text">
-              ユーザー名は3文字以上で入力してください。
+    
+            <div class="form-group">
+              <label for="users[email]">メールアドレス</label>
+              <input
+                v-model.trim="$v.user.email.$model"
+                type="text"
+                name="users[email]"
+                required
+              >
+              <div v-if="submitError && !$v.user.email.required" class="error-text">
+                メールアドレスは必須です。
+              </div>
+              <div v-if="submitError && !$v.user.email.email" class="error-text">
+                メールアドレスのフォーマットが不適切です。
+              </div>
             </div>
-            <div v-if="!$v.user.username.maxLength" class="error-text">
-              ユーザー名は50文字以下で入力してください。
+          
+            <div class="profile-form-btn">
+              <nui-button v-if="submitPending" class="btn-red1">
+                <i class="fad fa-spinner fa-spin fa-lg"></i>
+              </nui-button>
+              <nui-button v-else class="btn-red1" :submit="true">
+                変更する
+              </nui-button>
             </div>
-            <div v-if="!$v.user.username.alphaNumName" class="error-text">
-              ユーザー名は半角英数字及び_, -で入力してください。
-            </div>
-          </div>
-  
-          <div class="form-group">
-            <label for="users[email]">メールアドレス</label>
-            <input
-              v-model.trim="$v.user.email.$model"
-              type="text"
-              name="users[email]"
-              required
-            >
-            <div v-if="submitError && !$v.user.email.required" class="error-text">
-              メールアドレスは必須です。
-            </div>
-            <div v-if="submitError && !$v.user.email.email" class="error-text">
-              メールアドレスのフォーマットが不適切です。
-            </div>
-          </div>
-        
-          <div class="profile-form-btn">
-            <nui-button v-if="submitPending" class="btn-red1">
-              <i class="fad fa-spinner fa-spin fa-lg"></i>
-            </nui-button>
-            <nui-button v-else class="btn-red1" :submit="true">
-              変更する
-            </nui-button>
-          </div>
-        </form>
+          </form>
+          <nuxt-link to="/settings/quit" class="quit-link">アカウントを削除される場合はこちら</nuxt-link>
+        </div>
       </div>
     </main>
   </div>
@@ -167,6 +170,13 @@
   color: $color-teal1;
   left: 47%;
   position: absolute;
+}
+
+.quit-link {
+  float: right;
+  font-size: $font-size-xs;
+  margin: 5rem 0 4rem;
+  padding-right: 1.5rem;
 }
 </style>
 
