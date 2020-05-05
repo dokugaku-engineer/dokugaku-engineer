b3c57455af393671b90a3490a5948fb770d44501
kiyodori <kiyodori@yahoo.co.jp>
2020-05-06 07:50:18 +0900

    フッターに利用規約を追加、デザイン

--- client/components/layouts/Footer.vue
+++ client/components/layouts/Footer.vue
@@ -1,21 +1,71 @@
 <template>
-  <footer class="l-footer">
-    <p class="p-footer-copy">
-      ©Copyright 2019 独学エンジニア All rights reserved.
+  <footer class="footer">
+    <ul class="footer-list">
+      <li>
+        <nuxt-link to="/term">
+          利用規約
+        </nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/privacy">
+          プライバシーポリシー
+        </nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/tokutei">
+          特定商取引法に基づく表示
+        </nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/contact">
+          お問い合わせ
+        </nuxt-link>
+      </li>
+    </ul>
+    <p class="copyright">
+      © 2020 独学エンジニア
     </p>
   </footer>
 </template>
 
 <style lang="scss" scoped>
-.l-footer {
+.footer {
   background: $color-white1;
-  padding: 10px 0;
+  margin: 3rem 0 2rem;
   text-align: center;
 }
 
 @media screen and (min-width: 769px) {
-  .l-footer {
-    padding: 15px 0;
+  .footer {
+    margin: 5rem 0;
   }
+}
+
+.footer-list {
+  display: inline-block;
+  margin-bottom: 5rem;
+  vertical-align: middle;
+
+  li {
+    display: inline-block;
+    font-size: 1.3rem;
+    letter-spacing: 0.05em;
+    line-height: 1.8;
+    margin: 0 10px;
+  }
+}
+
+@media screen and (min-width: 769px) {
+  .footer-list {
+    li {
+      font-size: $font-size-sm;
+    }
+  }
+}
+
+.copyright {
+  font-size: 1rem;
+  letter-spacing: 0.05em;
+  padding-bottom: 2rem;
 }
 </style>
