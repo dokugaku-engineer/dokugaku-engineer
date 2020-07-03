b1ed5744789e361ce2c9281fc8078ab8467e433c
kiyodori <kiyodori@yahoo.co.jp>
2020-07-03 12:13:55 +0900

    存在しないパターンの条件分岐をしてそうなので削除

--- api/app/Helpers/DateHelper.php
+++ api/app/Helpers/DateHelper.php
@@ -9,17 +9,13 @@ class DateHelper
     /**
      * タイムスタンプを返す
      *
-     * @param Carbon|string|null $date
+     * @param Carbon|null $date
      * @return string|null
      */
     public static function getTimestamp($date): ?string
     {
         if (is_null($date)) {
             return null;
-        }
-
-        if (!$date instanceof Carbon) {
-            $date = Carbon::create($date);
         }
 
         return $date->format(config('api.timestamp_format'));
