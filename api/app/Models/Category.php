1f0b2339345e3119ccef2daf3589db0f1555d3df
kiyodori <kiyodori@yahoo.co.jp>
2020-07-04 07:08:42 +0900

    Model周りのLint対応

--- api/app/Models/Category.php
+++ api/app/Models/Category.php
@@ -6,12 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+    /**
+     * 複数代入しない属性
+     *
+     * @var array
+     */
     protected $guarded = ['id'];
 
+    /**
+     * 属性に対するモデルのデフォルト値
+     *
+     * @var array
+     */
     protected $attributes = [
         'parent' => 0,
     ];
 
+    /**
+     * カテゴリーに紐づく CategoryPost を取得
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
     public function category_post()
     {
         return $this->hasOne('App\Models\CategoryPost');
--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -8,13 +8,29 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class User extends Model
 {
     use SoftDeletes;
+
+    /**
+     * 複数代入しない属性
+     *
+     * @var array
+     */
     protected $guarded = ['id'];
 
+    /**
+     * ユーザーの受講コースを取得
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function taking_courses()
     {
         return $this->hasMany('App\Models\TakingCourse');
     }
 
+    /**
+     * ユーザーの受講履歴を取得
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function learning_histories()
     {
         return $this->hasMany('App\Models\LearningHistory');
